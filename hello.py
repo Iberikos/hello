e965ea667c698ec4852f82fceb01920ab0847044
Iberikos <Ruben89pm@gamil.com>
2021-09-14 23:50:13 +0200

    Inicial y final

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def el_que_os_de_la_gana():
+    return '¡Hola, mundo!'.upper()
+
+@app.route('/bye') 
+def otro():
+    return '!Adios, mundo cruel!'      
